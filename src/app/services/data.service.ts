865453bb679f663b51dd41dab679caf91a7d8544
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-07-27 07:33:22 -0500

    Format document

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-
-
 import { environment } from '../../environments/environment';
 
 @Injectable()
@@ -13,12 +7,11 @@ export class DataService {
 
   constructor(
     private http: HttpClient
-    ) { }
+  ) { }
 
   getData(type: string) {
-//    return this.http.get('https://jsonplaceholder.typicode.com/posts')
     return this.http.get(environment.apiUrl + type)
-      .subscribe( res => console.log(res)
+      .subscribe(res => console.log(res)
       );
 
   }
